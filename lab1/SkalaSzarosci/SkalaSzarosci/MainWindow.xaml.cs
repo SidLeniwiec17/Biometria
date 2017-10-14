312ec476ae9a5a30f606a2964e8fea90fda8f1c0
sidleniwiec17 <piotrmichalik17@wp.pl>
2017-10-14 19:01:50 +0200

    FirstLab Commit

---
+++ lab1/SkalaSzarosci/SkalaSzarosci/MainWindow.xaml.cs
@@ -0,0 +1,187 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SkalaSzarosci
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Bitmap newBmp;
+        public Bitmap originalBitmap;
+        public int darkBright;
+        public float contrast;
+        public int r;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            darkBright = 120;
+            contrast = 120;
+            r = 3;
+        }
+
+        private void Load_Button(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            dialog.Title = "pic";
+            dialog.Filter = "All supported graphics|*.jpg;*.jpeg;*.png;*.bmp|" +
+                "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+                "Portable Network Graphic (*.png)|*.png";
+
+            if (dialog.ShowDialog() == true)
+            {
+                img.Source = new BitmapImage(new Uri(dialog.FileName));
+                ori.Source = new BitmapImage(new Uri(dialog.FileName));
+                newBmp = (Bitmap)Bitmap.FromFile(dialog.FileName);
+                originalBitmap = (Bitmap)Bitmap.FromFile(dialog.FileName);
+            }
+        }
+
+        private async void DoGray_Button(object sender, RoutedEventArgs e)
+        {
+            if (newBmp == null)
+            {
+                MessageBox.Show("Load image!");
+                return;
+            }
+            BlakWait.Visibility = Visibility.Visible;
+            await RunGrayScale();
+            BlakWait.Visibility = Visibility.Collapsed;
+            img.Source = Methods.ToBitmapSource(newBmp);
+
+        }
+
+        public async Task RunGrayScale()
+        {
+            await Task.Run(() =>
+            {
+                newBmp = Methods.GrayScale(originalBitmap);
+            });
+        }
+
+        public async Task RunInverse()
+        {
+            await Task.Run(() =>
+            {
+                newBmp = Methods.Inverse(originalBitmap);
+            });
+        }
+
+        public async Task RunDarkBright()
+        {
+            await Task.Run(() =>
+            {
+                newBmp = Methods.DarkBright(originalBitmap, darkBright);
+            });
+        }
+        public async Task RunContrast()
+        {
+            await Task.Run(() =>
+            {
+                newBmp = Methods.Contrast(originalBitmap, contrast);
+            });
+        }
+
+        public async Task RunGlobBin()
+        {
+            await Task.Run(() =>
+            {
+                newBmp = Methods.GlobBin(originalBitmap, r);
+            });
+        }
+
+
+        private async void DoInverse_Button(object sender, RoutedEventArgs e)
+        {
+            if (newBmp == null)
+            {
+                MessageBox.Show("Load image!");
+                return;
+            }
+            BlakWait.Visibility = Visibility.Visible;
+            await RunInverse();
+            BlakWait.Visibility = Visibility.Collapsed;
+            img.Source = Methods.ToBitmapSource(newBmp);
+        }
+
+        private async void DarkBright_Button(object sender, RoutedEventArgs e)
+        {
+            if (newBmp == null)
+            {
+                MessageBox.Show("Load image!");
+                return;
+            }
+            BlakWait.Visibility = Visibility.Visible;
+            if(int.TryParse(dark.Text, out darkBright))
+            {
+                if(darkBright<=-255 || darkBright >= 255)
+                {
+                    MessageBox.Show("Incorrect darkBright!");
+                    return;
+                }
+            }
+            await RunDarkBright();
+            BlakWait.Visibility = Visibility.Collapsed;
+            img.Source = Methods.ToBitmapSource(newBmp);
+        }        
+
+        private async void Contrast_Button(object sender, RoutedEventArgs e)
+        {
+            if (newBmp == null)
+            {
+                MessageBox.Show("Load image!");
+                return;
+            }
+            BlakWait.Visibility = Visibility.Visible;
+            if (float.TryParse(contrast_txt.Text, out contrast))
+            {
+                if (contrast == 0)
+                {
+                    MessageBox.Show("Incorrect contrast!");
+                    return;
+                }
+            }
+            await RunContrast();
+            BlakWait.Visibility = Visibility.Collapsed;
+            img.Source = Methods.ToBitmapSource(newBmp);
+        }
+
+        private async void GlobalBin_Button(object sender, RoutedEventArgs e)
+        {
+            if (newBmp == null)
+            {
+                MessageBox.Show("Load image!");
+                return;
+            }
+            BlakWait.Visibility = Visibility.Visible;
+            if (int.TryParse(r_txt.Text, out r))
+            {
+                if (r < 0)
+                {
+                    MessageBox.Show("Incorrect contrast!");
+                    return;
+                }
+            }
+            await RunGlobBin();
+            BlakWait.Visibility = Visibility.Collapsed;
+            img.Source = Methods.ToBitmapSource(newBmp);
+        }
+    }
+}
---
+++ lab1/SkalaSzarosci/SkalaSzarosci/Methods.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace SkalaSzarosci
+{
+    public class Methods
+    {
+        public static Bitmap GrayScale(Bitmap btm)
+        {
+
+            Bitmap tempPict = new Bitmap(btm);
+            for (int x = 0; x < tempPict.Size.Width; x++)
+            {
+                for (int y = 0; y < tempPict.Size.Height; y++)
+                {
+                    System.Drawing.Color oldColour, newColor;
+                    oldColour = tempPict.GetPixel(x, y);
+                    var value = (oldColour.R + oldColour.G + oldColour.B) / 3;
+                    newColor = System.Drawing.Color.FromArgb(value, value, value);
+                    tempPict.SetPixel(x, y, newColor);
+                }
+            }
+
+            return tempPict;
+        }
+
+        public static Bitmap Inverse(Bitmap btm)
+        {
+
+            Bitmap tempPict = new Bitmap(btm);
+            for (int x = 0; x < tempPict.Size.Width; x++)
+            {
+                for (int y = 0; y < tempPict.Size.Height; y++)
+                {
+                    System.Drawing.Color oldColour, newColor;
+                    oldColour = tempPict.GetPixel(x, y);
+                    newColor = System.Drawing.Color.FromArgb(255 - oldColour.R, 255 - oldColour.G, 255 - oldColour.B);
+                    tempPict.SetPixel(x, y, newColor);
+                }
+            }
+
+            return tempPict;
+        }
+
+        public static Bitmap DarkBright(Bitmap btm, int darkBright)
+        {
+
+            Bitmap tempPict = new Bitmap(btm);
+            for (int x = 0; x < tempPict.Size.Width; x++)
+            {
+                for (int y = 0; y < tempPict.Size.Height; y++)
+                {
+                    System.Drawing.Color oldColour, newColor;
+                    oldColour = tempPict.GetPixel(x, y);
+                    int R = oldColour.R + darkBright;
+                    int G = oldColour.G + darkBright;
+                    int B = oldColour.B + darkBright;
+                    newColor = System.Drawing.Color.FromArgb(FromInterval(R), FromInterval(G), FromInterval(B));
+                    tempPict.SetPixel(x, y, newColor);
+                }
+            }
+
+            return tempPict;
+        }
+        public static Bitmap Contrast(Bitmap btm, float contrast)
+        {
+
+            Bitmap tempPict = new Bitmap(btm);
+            for (int x = 0; x < tempPict.Size.Width; x++)
+            {
+                for (int y = 0; y < tempPict.Size.Height; y++)
+                {
+                    System.Drawing.Color oldColour, newColor;
+                    oldColour = tempPict.GetPixel(x, y);
+                    int R = (int)(contrast * (oldColour.R - 127)) + 127;
+                    int G = (int)(contrast * (oldColour.G - 127)) + 127;
+                    int B = (int)(contrast * (oldColour.B - 127)) + 127;
+                    newColor = System.Drawing.Color.FromArgb(FromInterval(R), FromInterval(G), FromInterval(B));
+                    tempPict.SetPixel(x, y, newColor);
+                }
+            }
+
+            return tempPict;
+        }
+
+        public static Bitmap GlobBin(Bitmap btm, int r)
+        {
+            Bitmap tempPict = new Bitmap(btm);
+            tempPict = GrayScale(tempPict);
+            for (int x = 0; x < tempPict.Size.Width; x++)
+            {
+                for (int y = 0; y < tempPict.Size.Height; y++)
+                {
+                    System.Drawing.Color oldColour, newColor;
+                    oldColour = tempPict.GetPixel(x, y);
+                    int R = oldColour.R <= r ? 0 : 255 ;
+                    newColor = System.Drawing.Color.FromArgb(R,R,R);
+                    tempPict.SetPixel(x, y, newColor);
+                }
+            }
+
+            return tempPict;
+        }
+
+        private static int FromInterval(int col)
+        {
+            if (col > 255)
+                return 255;
+            if (col < 0)
+                return 0;
+            else
+                return col;
+        }
+
+        public static System.Windows.Media.ImageSource ToBitmapSource(Bitmap p_bitmap)
+        {
+            IntPtr hBitmap = p_bitmap.GetHbitmap();
+            System.Windows.Media.ImageSource wpfBitmap;
+            try
+            {
+                wpfBitmap = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(hBitmap, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally
+            {
+                //p_bitmap.Dispose();
+            }
+            return wpfBitmap;
+        }
+    }
+}
