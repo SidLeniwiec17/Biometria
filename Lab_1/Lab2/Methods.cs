e358a47185179d8d3df4efe36e43b982ad4113e2
sidleniwiec17 <piotrmichalik17@wp.pl>
2017-11-26 19:33:04 +0100

    second ab just started

---
+++ Lab_1/Lab2/Methods.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Lab2
+{
+    public class Methods
+    {
+        public static Bitmap GrayScale(Bitmap btm)
+        {
+            Bitmap tempPict = new Bitmap(btm);
+            for (int x = 0; x < tempPict.Size.Width; x++)
+            {
+                for (int y = 0; y < tempPict.Size.Height; y++)
+                {
+                    System.Drawing.Color oldColour, newColor;
+                    oldColour = tempPict.GetPixel(x, y);
+                    var value = (oldColour.R + oldColour.G + oldColour.B) / 3;
+                    newColor = System.Drawing.Color.FromArgb(value, value, value);
+                    tempPict.SetPixel(x, y, newColor);
+                }
+            }
+
+            return tempPict;
+        }
+
+        public static Tuple<Bitmap,int> GetGreyScaleTreshold(Bitmap btm)
+        {
+            int treshold = 0;
+            double hw = btm.Height * btm.Width;
+            Bitmap tempPict = new Bitmap(btm);
+            for (int x = 0; x < tempPict.Size.Width; x++)
+            {
+                for (int y = 0; y < tempPict.Size.Height; y++)
+                {
+                    System.Drawing.Color oldColour, newColor;
+                    oldColour = tempPict.GetPixel(x, y);
+                    var value = (oldColour.R + oldColour.G + oldColour.B) / 3;
+                    newColor = System.Drawing.Color.FromArgb(value, value, value);
+                    tempPict.SetPixel(x, y, newColor);
+                    treshold += (int)(newColor.R / hw);
+                }
+            }
+            return new Tuple<Bitmap, int>(tempPict, treshold);
+        }
+
+        public static System.Windows.Media.ImageSource ToBitmapSource(Bitmap p_bitmap)
+        {
+            IntPtr hBitmap = p_bitmap.GetHbitmap();
+            System.Windows.Media.ImageSource wpfBitmap;
+            try
+            {
+                wpfBitmap = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(hBitmap, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally
+            {
+                //p_bitmap.Dispose();
+            }
+            return wpfBitmap;
+        }
+    }
+}
